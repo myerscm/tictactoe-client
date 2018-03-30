7c04478d967731804be5e7e2176d95a433d2e520
myerscm <myerscm@kenyon.edu>
2018-03-30 13:16:01 -0400

    Add functioning Curl scripts
    Curl Scripts added to auth branch

---
+++ curl-scripts/change-password.sh
@@ -0,0 +1,13 @@
+curl "http://tic-tac-toe.wdibos.com/change-password/:id" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLD}"'",
+      "new": "'"${NEW}"'"
+    }
+  }'
+
+echo
