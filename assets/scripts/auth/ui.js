4477eca1df4133a6770cba5e0323389ba3a30b30
myerscm <myerscm@kenyon.edu>
2018-04-04 14:50:56 -0400

    Add UI features, finish game board/logic
    Add alert messages for player turns, win conditions, auth success and
    failure, taken spaces etc. New Game button works. game logic working
    seamlessly.
    up next, hiding changepass and signout until successful sign in/sign up

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,42 +1,42 @@
 'use strict'
 const store = require('../store')
 const signUpSuccess = function () {
-  $('#message').text('Successfully signed up')
-  $('#message').css('background-color', 'green')
+  $('#authmessage').html('Successfully signed up')
+  $('#authmessage').css('background-color', 'green')
 }
 const signInSuccess = function (data) {
-  $('#message').text('Successfully signed in')
-  $('#message').css('background-color', 'green')
-  $('#message').css('color', 'white')
-  $('#message').css('text-align', 'center')
+  $('#authmessage').html('Successfully signed in')
+  $('#authmessage').css('background-color', 'green')
+  $('#authmessage').css('color', 'white')
+  $('#authmessage').css('text-align', 'center')
   console.log(data)
   store.user = data.user
 }
 
 const signUpFailure = function () {
-  $('#message').text('Failure signing up')
-  $('#message').css('background-color', 'red')
+  $('#authmessage').html('Failure signing up')
+  $('#authmessage').css('background-color', 'red')
 }
 const signInFailure = function () {
-  $('#message').text('Failure signing in')
-  $('#message').css('background-color', 'red')
+  $('#authmessage').html('Failure signing in')
+  $('#authmessage').css('background-color', 'red')
 }
 const changePasswordSuccess = function () {
-  $('#message').text('Successfully changed password')
-  $('#message').css('background-color', 'green')
+  $('#authmessage').html('Successfully changed password')
+  $('#authmessage').css('background-color', 'green')
 }
 const changePasswordFailure = function () {
-  $('#message').text('Failure changing password')
-  $('#message').css('background-color', 'red')
+  $('#authmessage').html('Failure changing password')
+  $('#authmessage').css('background-color', 'red')
 }
 const signOutSuccess = function () {
-  $('#message').text('Successfully signed out')
-  $('#message').css('background-color', 'green')
+  $('#authmessage').html('Successfully signed out')
+  $('#authmessage').css('background-color', 'green')
   store.user = null
 }
 const signOutFailure = function () {
-  $('#message').text('Failure signing out')
-  $('#message').css('background-color', 'red')
+  $('#authmessage').html('Failure signing out')
+  $('#authmessage').css('background-color', 'red')
 }
 module.exports = {
   signUpSuccess,
--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -4,12 +4,16 @@ const ui = require('./ui')
 
 const player1 = 'X'
 const player2 = 'O'
-const board = ['', '', '', '', '', '', '', '', '']
+let board = ['', '', '', '', '', '', '', '', '']
 let turn = ''
-const newGame = function () {
-  for (let i = 0; i < board.length; i++) {
-    board[i].innerHTML = []
-  }
+const newGame = function (event) {
+  event.preventDefault()
+  $('.game-board').show()
+  $('.box').html('')
+  board = ['', '', '', '', '', '', '', '', '']
+  turn = player2
+  $('#message').html('Player X, Begin!')
+  $('#message').css('background-color', 'white')
 }
 const setArray = function () {
   board[0] = $('#box-0').text()
@@ -34,35 +38,58 @@ const winCondition = function () {
   board[7] = $('#box-7').text()
   board[8] = $('#box-8').text()
 
-  if (board[0] !== '' && board[0] === board[1] && board[1] === board[2]) {
-    console.log('You Have Won!')
-  } else if (board[3] !== '' && board[3] === board[4] && board[3] === board[5]) {
-    console.log('You Have Won!')
-  } else if (board[6] !== '' && board[6] === board[7] && board[6] === board[8]) {
-    console.log('You Have Won!')
-  } else if (board[0] !== '' && board[0] === board[3] && board[0] === board[6]) {
-    console.log('You Have Won!')
-  } else if (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) {
-    console.log('You Have Won!')
-  } else if (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) {
-    console.log('You Have Won!')
-  } else if (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) {
-    console.log('You Have Won!')
-  } else if (board[2] !== '' && board[2] === board[4] && board[2] === board[6]) {
-    console.log('You Have Won!')
+  if ((board[0] === 'X' && board[0] === board[1] && board[1] === board[2]) ||
+  (board[3] === 'X' && board[3] === board[4] && board[3] === board[5]) ||
+  (board[6] === 'X' && board[6] === board[7] && board[6] === board[8]) ||
+  (board[0] === 'X' && board[0] === board[3] && board[0] === board[6]) ||
+  (board[1] === 'X' && board[1] === board[4] && board[1] === board[7]) ||
+  (board[2] === 'X' && board[2] === board[5] && board[2] === board[8]) ||
+  (board[0] === 'X' && board[0] === board[4] && board[0] === board[8]) ||
+  (board[2] === 'X' && board[2] === board[4] && board[2] === board[6])) {
+    console.log('X wins')
+    $('#message').html('Player X wins!')
+    $('#message').css('background-color', 'green')
+    $('.game-board').hide()
+    return true
+  } else if ((board[0] === 'O' && board[0] === board[1] && board[1] === board[2]) ||
+(board[3] === 'O' && board[3] === board[4] && board[3] === board[5]) ||
+(board[6] === 'O' && board[6] === board[7] && board[6] === board[8]) ||
+(board[0] === 'O' && board[0] === board[3] && board[0] === board[6]) ||
+(board[1] === 'O' && board[1] === board[4] && board[1] === board[7]) ||
+(board[2] === 'O' && board[2] === board[5] && board[2] === board[8]) ||
+(board[0] === 'O' && board[0] === board[4] && board[0] === board[8]) ||
+(board[2] === 'O' && board[2] === board[4] && board[2] === board[6])) {
+    console.log('O wins')
+    $('#message').html('Player O wins!')
+    $('#message').css('background-color', 'blue')
+    $('.game-board').hide()
+    return true
+  } else if ((board[0] !== '' && board[1] !== '' && board[2] !== '' &&
+    board[3] !== '' && board[4] !== '' && board[5] !== '' && board[6] !== '' &&
+    board[7] !== '' && board[8] !== '')) {
+    console.log('Its a draw')
+    $('#message').html('Its a draw!')
+    $('#message').css('background-color', 'purple')
+    $('.game-board').hide()
   }
 }
 const turnSwitch = function (e) {
   if (e.target.innerHTML !== 'X' && e.target.innerHTML !== 'O') {
     if (turn === player1) {
       turn = player2
+      $('#message').html('Turn belongs to Player X')
+      $('#message').css('background-color', 'white')
     } else {
       turn = player1
+      $('#message').html('Turn belongs to Player O')
+      $('#message').css('background-color', 'white')
     }
     e.target.innerHTML = turn
     console.log(turn)
   } else {
     console.log('Not a valid space')
+    $('#message').html('This space is taken!')
+    $('#message').css('background-color', 'red')
   }
   return turn
 }
