f5432851ff2a4616fb36d3f9228a3a7b2dcc2122
myerscm <myerscm@kenyon.edu>
2018-04-04 16:12:50 -0400

    Complete auth and game w/o API integration
    testing first merge and first deploy with this version. All reqs beside
    api reqs are met.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,14 +1,25 @@
 'use strict'
 const store = require('../store')
 const signUpSuccess = function () {
-  $('#authmessage').html('Successfully signed up')
+  $('#authmessage').html('Successfully signed up! Sign in to continue')
   $('#authmessage').css('background-color', 'green')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 8000
+  )
+  $('#togglesu').addClass('hidden')
 }
 const signInSuccess = function (data) {
-  $('#authmessage').html('Successfully signed in')
+  $('#authmessage').html('Successfully signed in!')
   $('#authmessage').css('background-color', 'green')
-  $('#authmessage').css('color', 'white')
-  $('#authmessage').css('text-align', 'center')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
+  $('#togglesu').addClass('hidden')
+  $('#togglesi').addClass('hidden')
+  $('#togglecp').removeClass('hidden')
+  $('#toggleso').removeClass('hidden')
   console.log(data)
   store.user = data.user
 }
@@ -16,27 +27,55 @@ const signInSuccess = function (data) {
 const signUpFailure = function () {
   $('#authmessage').html('Failure signing up')
   $('#authmessage').css('background-color', 'red')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
 }
 const signInFailure = function () {
   $('#authmessage').html('Failure signing in')
   $('#authmessage').css('background-color', 'red')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
 }
 const changePasswordSuccess = function () {
   $('#authmessage').html('Successfully changed password')
   $('#authmessage').css('background-color', 'green')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
 }
 const changePasswordFailure = function () {
   $('#authmessage').html('Failure changing password')
   $('#authmessage').css('background-color', 'red')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
 }
 const signOutSuccess = function () {
   $('#authmessage').html('Successfully signed out')
   $('#authmessage').css('background-color', 'green')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
+  $('#togglesu').removeClass('hidden')
+  $('#togglesi').removeClass('hidden')
+  $('#togglecp').addClass('hidden')
+  $('#toggleso').addClass('hidden')
   store.user = null
 }
 const signOutFailure = function () {
   $('#authmessage').html('Failure signing out')
   $('#authmessage').css('background-color', 'red')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
 }
 module.exports = {
   signUpSuccess,
